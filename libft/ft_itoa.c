5eac73449bdde38435b186f6eced55d19f6e550c
hvan-hov <hjg.van.hove@gmail.com>
2021-10-28 19:49:15 +0100

    28 oct

---
+++ libft/ft_itoa.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 15:20:47 by hvan-hov          #+#    #+#             */
+/*   Updated: 2021/10/28 16:58:57 by hvan-hov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int	int_len(int i)
+{
+	size_t	len;
+
+	len = 0;
+	while (i > 9 || i < -9)
+	{
+		i /= 10;
+		len++;
+	}
+	return (len + 1);
+}
+
+static char	*itoa_write(char *dst, int n, int len, int offset)
+{
+	while (n > 9)
+	{
+		dst[len + offset - 1] = (n % 10) + '0';
+		n /= 10;
+		len--;
+	}
+	dst[len + offset - 1] = n + '0';
+	return (dst);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+	int		len;
+	int		offset;
+
+	len = int_len(n);
+	offset = 0;
+	if (n < 0)
+		offset = 1;
+	result = (char *)malloc((len + offset) * sizeof(char));
+	if (n == -2147483648)
+	{
+		result[1] = '2';
+		n = -147483648;
+	}
+	if (n < 0)
+	{
+		result[0] = '-';
+		n = n * -1;
+	}
+	result = itoa_write(result, n, len, offset);
+	return (result);
+}
+
+/*
+int	main(void)
+{
+	int	i = 1234;
+	int j = -1234;
+	int k = 0;
+	int l = 2147483647;
+	int m = -2147483648;
+
+	printf("The result of %d in itoa is: %s\n",i ,ft_itoa(i));
+	printf("The result of %d in itoa is: %s\n",j ,ft_itoa(j));
+	printf("The result of %d in itoa is: %s\n",k ,ft_itoa(k));
+	printf("The result of %d in itoa is: %s\n",l ,ft_itoa(l));
+	printf("The result of %d in itoa is: %s\n",m ,ft_itoa(m));
+
+}
+*/
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,16 +6,23 @@
 /*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:50:42 by hvan-hov          #+#    #+#             */
-/*   Updated: 2021/10/27 22:00:34 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2021/10/28 15:20:27 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-static size_t	word_c(const char *str, char c)
+static int	free_mem(char **split, int len)
 {
-	size_t count;
+	while (len--)
+		free(split[len]);
+	return (-1);
+}
+
+static size_t	word_count(const char *str, char c)
+{
+	size_t	count;
 
 	count = 0;
 	while (*str != '\0')
@@ -33,68 +40,87 @@ static size_t	word_c(const char *str, char c)
 	return (count);
 }
 
-char **ft_split(char const *s, char c)
+static void	write_word(char *dst, const char *src, char c)
 {
-	char	**split;
 	size_t	i;
-	size_t	j;
-	size_t	len;
 
-	if ((split = (char **)malloc((word_c(s, c) + 1) * sizeof(char *))) == NULL)
-		return (NULL);
 	i = 0;
-	while (*s)
+	while (src[i] != c && src[i] != '\0')
 	{
-		if (*s == c)
-			s++;
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+static int	write_split(char **split, const char *s, char c)
+{
+	size_t	i;
+	size_t	len;
+	size_t	wc;
+
+	i = 0;
+	wc = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
 		else
 		{
-			// write word function  
-
 			len = 0;
-			while (s[len] != c && s[len] != '\0')
+			while (s[i + len] != c && s[i + len] != '\0')
 				len++;
-			if ((split[i] = (char *)malloc((len + 1) * sizeof(char))) == NULL)
-				return (NULL);
-			j = 0;
-			while (j < len)
-			{
-				split[i][j] = s[j];
-				j++;
-			}
-			split[i][j] = '\0';
-			s += len;
-			i++;
+			split[wc] = (char *)malloc((len + 1) * sizeof(char));
+			if (split[wc] == NULL)
+				return (free_mem(split, wc - 1));
+			write_word(split[wc], s + i, c);
+			i += len;
+			wc++;
 		}
 	}
-	split[i] = NULL;
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	wc;
+
+	wc = word_count(s, c);
+	split = (char **)malloc((wc + 1) * sizeof(char *));
+	if (split == NULL)
+		return (NULL);
+	if (write_split(split, s, c) == -1)
+		return (NULL);
+	split[wc] = NULL;
 	return (split);
 }
 
+/*
 int main(void)
 {
 	char	str1[] = "___hello_there__dude___";
 	char	str2[] = "**Hello*there**friend**!!!";
-	char	str3[] = "thisisatest";
+	char	str3[] = "____";
 	char	str4[] = "";
 	char	c1 = '_';
 	char	c2 = '*';
 
 	printf("TESTING...\n");
-	/*
+	printf("The string '%s' gives: \n", str1);
 	for (int i = 0; i < 4; i++)
 		printf("splitted[%d] is: %s\n", i, ft_split(str1, c1)[i]);
 	printf("---\n");
-	*/
-	for (int i = 0; i < 4; i++)
+	printf("The string '%s' gives: \n", str2);
+	for (int i = 0; i < 5; i++)
 		printf("splitted[%d] is: %s\n", i, ft_split(str2, c2)[i]);
 	printf("---\n");
-	/*
-	for (int i = 0; i < 3; i++)
+	printf("The string '%s' gives: \n", str3);
+	for (int i = 0; i < 2; i++)
 		printf("splitted[%d] is: %s\n", i, ft_split(str3, c1)[i]);
-	printf("---\n");
-	for (int i = 0; i < 3; i++)
+	printf("---\n");	
+	printf("The string '%s' gives: \n", str4);
+	for (int i = 0; i < 2; i++)
 		printf("splitted[%d] is: %s\n", i, ft_split(str4, c1)[i]);
 	printf("---\n");
-	*/
-}
+}*/
