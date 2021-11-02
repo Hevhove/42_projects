61ff1eba492d0f2e52a0bf2895787232de2ac570
hvan-hov <hjg.van.hove@gmail.com>
2021-11-02 17:54:46 +0000

    1st submit

---
+++ libft/libft_submit/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 15:27:31 by hvan-hov          #+#    #+#             */
+/*   Updated: 2021/10/30 18:11:34 by hvan-hov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr != NULL)
+		memset(ptr, 0, count * size);
+	return (ptr);
+}
