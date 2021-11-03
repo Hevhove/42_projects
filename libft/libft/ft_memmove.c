ab276c9a59759f0049136ea865e0d624d47cef30
hvan-hov <hjg.van.hove@gmail.com>
2021-11-03 09:28:50 +0000

    2nd push to moulinette

---
+++ libft/libft/ft_memmove.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memmove.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 17:42:30 by hvan-hov          #+#    #+#             */
+/*   Updated: 2021/11/01 15:37:34 by hvan-hov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+	char	*lasts;
+	char	*lastd;
+
+	if (!dst && !src)
+		return (NULL);
+	d = (char *)dst;
+	s = (char *)src;
+	if (d < s)
+		while (n--)
+			*d++ = *s++;
+	else
+	{
+		lasts = s + (n - 1);
+		lastd = d + (n - 1);
+		while (n--)
+			*lastd-- = *lasts--;
+	}
+	return (dst);
+}
+
+/*
+int	main(void)
+{
+	char src1[] = "Hello";
+	char dst1[] = "XXXXX";
+	int src2[] = {1, 2, 3, 4, 5};
+	int dst2[] = {0, 0, 0, 0, 0};
+
+	char *dst3 = src1 + ;
+
+	printf("Current dst is: %s\n", dst1);
+	ft_memmove(dst3, src1, 3);
+	printf("New dst is: %s\n", dst1);
+
+	for (int i = 0; i < 5; i++)
+		printf("current dst[i] is: %d\n", dst2[i]);
+	memmove(dst2, src2, 5);
+	for (int i = 0; i < 5; i++)
+		printf("new dst[i] is: %d\n", dst2[i]);
+}
+*/
---
+++ libft/libft/ft_strnstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 17:33:24 by hvan-hov          #+#    #+#             */
+/*   Updated: 2021/11/01 18:14:24 by hvan-hov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*needle == '\0' || needle == NULL)
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j] && i + j < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+int main(void)
+{
+	char needle[] = "Caz";
+	char haystack[] = "Foo Bar Caz";
+
+	printf("The first occurence of '%s' in
+   	'%s' is at %c\n", needle, haystack, *ft_strnstr(haystack, needle, 11));
+}
+*/
