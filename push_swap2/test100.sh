7c38f5245594984d1a971745384ccaf21d87fd6a
Hendrik Van Hove <hjg.van.hove@gmail.com>
2022-02-08 16:53:57 +0000

    Delete push_swap2 directory

--- push_swap2/test100.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..25}
-do
-	echo "-|-|-|-|-|-|-|-|-|-|-"
-	ARG=`ruby -e "puts (-500..500).to_a.shuffle.first(100).join(' ')"`;
-	./push_swap $ARG | ./checker_Mac $ARG
-	./push_swap $ARG | wc -l
-done
