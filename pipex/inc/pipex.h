6480604d4f664cace5236086feb968dba9038cf8
hvan-hov <hjg.van.hove@gmail.com>
2022-02-18 11:52:01 +0000

    first working version

--- pipex/inc/pipex.h
+++ pipex/inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 14:20:39 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/02/17 19:31:38 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/02/17 20:43:59 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(const char *s, char c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
 
 #endif
--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,17 +6,11 @@
 /*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 10:29:49 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/02/17 20:03:15 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/02/18 11:51:34 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
-
-// ALLOWED FUNCTIONS: 
-// open, close, read, write, malloc, free
-// perror, strerror, access, dup, dup2, execve
-// exit, fork, pipe, unlink, wait, waitpid
-// ft_printf
 
 char	*find_path(char **envp)
 {
@@ -25,11 +19,12 @@ char	*find_path(char **envp)
 	return (*envp + 5);
 }
 
-void	child1(int f1, char **argv, int fd[2], char **envp)
+void	child1(int f1, char *cmd_in, int fd[2], char **envp)
 {
 	char	**paths;
 	char	**cmdargs;
-	char	*cmd;
+	char	*cmd_path;
+	char	*inter;
 	int		i;
 	
 	dup2(f1, STDIN_FILENO);
@@ -38,15 +33,45 @@ void	child1(int f1, char **argv, int fd[2], char **envp)
 	close(f1);
 	// maybe still close fd[1]?
 	paths = ft_split(find_path(envp), ':');
-	cmdargs = ft_split(argv[2], " ");
-	i = 0;
-	while (paths[i])
+	cmdargs = ft_split(cmd_in, ' ');
+	i = -1;
+	while (paths[++i])
 	{
-		cmd = ft_strjoin(paths[i], argv[2]); // TODO: PROTECT THIS!!
-		execve(cmd, cmdargs, envp); // if this succeeds, it will take over the entire process;
-		free(cmd);
+		inter = ft_strjoin(paths[i], "/");
+		cmd_path = ft_strjoin(inter, cmdargs[0]); // TODO: PROTECT THIS!!
+		free(inter);
+		// ft_printf("Currently checking: %s\n", cmd);
+		execve(cmd_path, cmdargs, envp); // if this succeeds, it will take over the entire process;
+		free(cmd_path);
 	}
-	return (1); // error code if we fail
+	exit(1); // error code if we fail
+}
+
+void	child2(int f2, char *cmd_in, int fd[2], char **envp)
+{
+	char	**paths;
+	char	**cmdargs;
+	char	*cmd_path;
+	char	*inter;
+	int		i;
+	
+	dup2(fd[0], STDIN_FILENO);
+	dup2(f2, STDOUT_FILENO);
+	close(fd[1]); // do we close in this scope or in pipex scope?
+	close(f2);
+	// maybe still close fd[1]?
+	paths = ft_split(find_path(envp), ':');
+	cmdargs = ft_split(cmd_in, ' ');
+	i = -1;
+	while (paths[++i])
+	{
+		inter = ft_strjoin(paths[i], "/");
+		cmd_path = ft_strjoin(inter, cmdargs[0]); // TODO: PROTECT THIS!!
+		free(inter);
+		execve(cmd_path, cmdargs, envp); // if this succeeds, it will take over the entire process;
+		free(cmd_path);
+	}
+	exit(1); // error code if we fail
 }
 
 void	pipex(int f1, int f2, char **argv, char **envp)
@@ -64,12 +89,14 @@ void	pipex(int f1, int f2, char **argv, char **envp)
 	if (pid1 < 0)
 		return (perror("Fork error: "));
 	if (pid1 == 0)
-		child1(f1, argv, fd, envp);
+		child1(f1, argv[2], fd, envp);
 	pid2 = fork();
 	if (pid2 < 0)
 		return (perror("Fork error: "));
 	if (pid2 == 0)
-		child2(f2, argv, fd, envp);
+		child2(f2, argv[3], fd, envp);
+	close(fd[0]);
+	close(fd[1]);
 	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
 }
@@ -78,31 +105,16 @@ int main(int argc, char **argv, char **envp)
 {
 	int		f1;
 	int		f2;
-	// char	**paths;
 	
-	// ./pipex infile "ls -l" "wc -l" outfile
-
-	// if (argc != 5)
-	// {
-	// 	ft_printf("Usage : .pipex infile cmd1 cmd2 outfile");
-	// 	return (1);
-	// }
+	if (argc != 5)
+	{
+		ft_printf("Usage : .pipex infile cmd1 cmd2 outfile");
+		return (1);
+	}
 	f1 = open(argv[1], O_RDONLY);
 	f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (f1 < 0 || f2 < 0)
 		return (-1);
-	// paths = ft_split(find_path(envp), ':');
-	pipex(f1, f2, argv + 1, envp);
-	if (pid1 == 0) // CHILD PROCESS
-	{
-		if (execve("/bin/ls", argv, envp) == -1)
-		{
-			ft_printf("error");
-			return (2);
-		}
-		// this process is now taken over and terminates
-	}
-	// no need for an else because process will be taken over
-	// above and only parent will be running
+	pipex(f1, f2, argv, envp);
 	return (0);
 }
