e791fa6625999293fb660ba1a6da41b507e7da70
hvan-hov <hjg.van.hove@gmail.com>
2022-03-25 20:19:03 +0000

    more steps

--- philosophers/inc/philo.h
+++ philosophers/inc/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Hendrik <Hendrik@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:12:39 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/03/25 17:16:34 by Hendrik          ###   ########.fr       */
+/*   Updated: 2022/03/25 19:42:48 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,27 @@
 #include <sys/time.h>
 
 // STRUCTS
-typedef struct s_data {
-	int num_phil;
-	int t_die; // in microseconds
-	int t_eat; // in microseconds
-	int t_sleep; // in microseconds
-	int	must_check;
-	int must_num;
-}	t_data;
 
 typedef struct s_philo {
 	int			id;
 	int			fork;
 	char		state;
-	p_thread_t	th;
+	pthread_t	th;
 }	t_philo;
+
+typedef struct s_data {
+	int 			num_phil;
+	int 			t_die; // in microseconds
+	int 			t_eat; // in microseconds
+	int 			t_sleep; // in microseconds
+	int				must_check;
+	int 			must_num;
+	int				id_pass;
+	t_philo			*philos;
+	pthread_mutex_t	mutex;
+	int				check;
+}	t_data;
+
 
 // FUNCTION PROTOTYPES
 int	ft_atoi(const char *str);
--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Hendrik <Hendrik@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 17:57:07 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/03/25 17:26:03 by Hendrik          ###   ########.fr       */
+/*   Updated: 2022/03/25 20:12:59 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+
+void	error_message(const char *msg, int exit_code)
+{
+	printf("%s\n", msg);
+	exit(exit_code);
+}
 
 unsigned long long get_time_ms()
 {
@@ -22,7 +28,7 @@ unsigned long long get_time_ms()
 	return (curtime);
 }
 
-void arg_check(char **argv, t_data *data, t_philo **philos)
+void arg_check(char **argv, t_data *data)
 {
 	int i;
 
@@ -35,48 +41,76 @@ void arg_check(char **argv, t_data *data, t_philo **philos)
 		data->must_check = 1;
 		data->must_num = ft_atoi(argv[5]);
 	}
-	*philos = malloc(data->num_phil * sizeof(t_philo));
+	data->philos = malloc(data->num_phil * sizeof(t_philo));
 	i = 0;
 	while (i < data->num_phil)
 	{
-		(*philos)[i].id = i;
-		(*philos)[i].fork = 1;
-		(*philos)[i].state = 's';
+		data->philos[i].id = i;
+		data->philos[i].fork = 1;
+		data->philos[i].state = 's';
 		i++;
 	}
 }
 
-
-
-void	philo(t_data *data, t_philo **philos)
+void	start_eating(t_data *data)
 {
-	int				i;
-	pthread_mutex_t	mutex;
-
-	i = 0;
-	pthread_mutex_init(&mutex, NULL);
-	while (i < data->num_phil)
-	{
-		if (pthread_create(&(*philos)[i].th, NULL, &routine, NULL) != 0)
-			return (1);
-
-	}
+	if (data->philos[data->philos->id].fork == 1 
+		&& data->philos[(data->philos->id + 1) % 5].fork == 1)
+		
 }
 
-void	error_message(const char *msg, int exit_code)
+void	*routine(void *arg)
 {
-	printf("%s\n", msg);
-	exit(exit_code);
+	int	i;
+	t_data *data;
+	
+	i = 0;
+	data = (t_data *)arg;
+	pthread_mutex_lock(&(data->mutex));
+	while (1)
+	{
+		start_eating(data);
+	}
+	data->check++;
+	pthread_mutex_unlock(&(data->mutex));
+	return (NULL);
+}
+
+void	philo(t_data *data)
+{
+	int				i;
+	int				*a;
+	i = 0;
+	pthread_mutex_init(&(data->mutex), NULL);
+	while (i < data->num_phil)
+	{
+		a = malloc(sizeof(int));
+		*a = i;
+		data->philos[*a].id = *a;
+		if (pthread_create(&(data->philos[i].th), NULL, &routine, data) != 0)
+			error_message("failed to create threads", 2);
+		i++;
+		//printf("check\n");
+	}
+	i = 0;
+	while (i < data->num_phil)
+	{
+		if ((pthread_join(data->philos[i].th, NULL)) != 0)
+			error_message("failed to join threads", 3);
+		//printf("check2\n");
+		i++;
+	}
+	pthread_mutex_destroy(&(data->mutex));
 }
 
 int	main(int argc, char **argv)
 {
 	t_data data;
-	t_philo *philos;
 
+	data.check = 0;
 	if (argc != 5 && argc != 6)
 		error_message("Not enough arguments", 1);
-	arg_check(argv, &data, &philos);
-	philo(&data, &philos);
-
+	arg_check(argv, &data);
+	philo(&data);
+	printf("data check is: %d\n", data.check);
 }
