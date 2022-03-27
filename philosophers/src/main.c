fec69f1fe50e2012150a6eb5cbea9356e1e9bde5
Hendrik <hjg.van.hove@gmail.com>
2022-03-27 15:41:27 +0100

    changes to deaths

--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Hendrik <Hendrik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 17:57:07 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/03/26 19:59:57 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/03/27 14:51:26 by Hendrik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void	error_message(const char *msg, int exit_code)
 	exit(exit_code);
 }
 
-unsigned long long get_time_ms()
+unsigned long long get_time_micros()
 {
 	struct timeval		tv;
 	unsigned long long	curtime;
 
 	gettimeofday(&tv, NULL);
-	curtime = (unsigned long long)((tv.tv_sec * 1000000 + tv.tv_usec) / 1000);
+	curtime = (unsigned long long)((tv.tv_sec * 1000000 + tv.tv_usec));
 	return (curtime); // in microseconds
 }
 
@@ -48,12 +48,11 @@ int	grab_forks(t_philo *phil, t_philo *next_phil)
 
 void	philo_think(t_philo *phil, t_philo *next_phil)
 {
-	// printf("%llu: philosopher %d is thinking\n", get_time_ms(), phil->id);
 	(void)next_phil;
 	if (grab_forks(phil, next_phil) == 1)
 	{
 		printf("%llu: philosopher %d has grabbed forks %d and %d\n",
-		get_time_ms(), phil->id, phil->fork.owner_id, next_phil->fork.owner_id);
+		get_time_micros() / 1000, phil->id + 1, phil->fork.owner_id + 1, next_phil->fork.owner_id + 1);
 		phil->p_state = EATING;
 	}
 }
@@ -73,21 +72,21 @@ void	drop_forks(t_philo *phil, t_philo *next_phil)
 
 void	philo_eat(t_philo *phil, t_philo *next_phil)
 {
-	printf("%llu: philosopher %d is eating\n", get_time_ms(), phil->id);
+	printf("%llu: philosopher %d is eating\n", get_time_micros() / 1000, phil->id + 1);
 	phil->times_eaten++;
-	phil->last_eaten = get_time_ms();
+	phil->last_eaten = get_time_micros();
 	usleep(phil->data->t_eat * 1000);
 	drop_forks(phil, next_phil);
-	printf("%llu: philosopher %d has stopped eating\n", get_time_ms(), phil->id);
+	printf("%llu: philosopher %d has stopped eating\n", get_time_micros() / 1000, phil->id + 1);
 	phil->p_state = SLEEPING;
 }
 
 void	philo_sleep(t_philo *phil)
 {
-	printf("%llu: philosopher %d is sleeping\n", get_time_ms(), phil->id);
+	printf("%llu: philosopher %d is sleeping\n", get_time_micros() / 1000, phil->id + 1);
 	usleep(phil->data->t_sleep * 1000);
 	phil->p_state = THINKING;
-	printf("%llu: philosopher %d is thinking\n", get_time_ms(), phil->id);
+	printf("%llu: philosopher %d is thinking\n", get_time_micros() / 1000, phil->id + 1);
 }
 
 int	check_eaten(t_data *data)
@@ -110,12 +109,16 @@ int	check_death(t_philo *phil)
 {
 	if (phil->times_eaten != 0)
 	{
-		if (get_time_ms() - phil->last_eaten > (phil->data->t_die * 1000))
+		printf("phil %d: diff is %llu\n", phil->id + 1, get_time_micros() - phil->last_eaten);
+		printf("last eaten time is: %llu\n", phil->last_eaten);
+		if (get_time_micros() - phil->last_eaten > (phil->data->t_die * 1000))
+		{
 			return (1);
+		}
 	}
 	else
 	{
-		if (get_time_ms() - phil->data->start_time > (phil->data->t_die * 1000))
+		if (get_time_micros() - phil->data->start_time > (phil->data->t_die * 1000))
 			return (1);
 	}
 	return (0);
@@ -126,7 +129,7 @@ void	*routine(void *arg)
 	int	i;
 	t_philo *phil;
 	t_philo	*next_phil;
-	
+
 	i = 0;
 	phil = (t_philo *)arg;
 	next_phil = phil->data->philos + ((phil->id + 1) % phil->data->num_phil);
@@ -134,7 +137,7 @@ void	*routine(void *arg)
 	{
 		if (check_death(phil) == 1)
 		{
-			printf("philo %d has died!\nSimulation stopped!\n", phil->id);
+			printf("philo %d has died!\nSimulation stopped!\n", phil->id + 1);
 			exit(5);
 		}
 		if (phil->p_state == THINKING)
@@ -160,7 +163,7 @@ void	*routine(void *arg)
 void	philo(t_data *data)
 {
 	int				i;
-	
+
 	i = 0;
 	pthread_mutex_init(&(data->mutex), NULL);
 	while (i < data->num_phil)
@@ -188,7 +191,7 @@ int	main(int argc, char **argv)
 		error_message("Not enough arguments", 1);
 	arg_check(argv, &data);
 	data.state = RUNNING;
-	data.start_time = get_time_ms();
+	data.start_time = get_time_micros();
 	philo(&data);
 	printf("data check is: %d\n", data.check);
 }
