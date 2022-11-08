08680a98c1acdf7fc87239c0873adebb82030740
Hendrik jacques g Van hove <hvan-hov@student.42lisboa.com>
2022-11-08 12:55:49 +0000

    cleanup: libft Makefile

--- libft/srcs/ft_strmapi.c
+++ libft/srcs/ft_strmapi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvan-hov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:59:25 by hvan-hov          #+#    #+#             */
-/*   Updated: 2021/11/01 15:43:22 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/11/08 12:19:04 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	len;
 	char	*result;
 	int		i;
 
 	if (!s || !f)
 		return (NULL);
-	len = ft_strlen(s);
 	result = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
