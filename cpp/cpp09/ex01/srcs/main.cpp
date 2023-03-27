749e573c6e9d7d6b1df044e2ece4dc2f00c57b68
Hendrik jacques g Van hove <hvan-hov@student.42lisboa.com>
2023-03-27 19:55:07 +0100

    fix: added fix for empty string

--- cpp/cpp09/ex01/srcs/main.cpp
+++ cpp/cpp09/ex01/srcs/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     // Check arguments
-    if (argc != 2)
+    if (argc != 2 || argv[1][0] == '\0')
     {
         std::cerr << "Error" << std::endl;
         return (-1);
